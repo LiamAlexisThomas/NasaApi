84f11e20236f62cb9be8f19fc5c9272bc5aa0eed
LiamAlexisThomas <liamalexisthomas@gmail.com>
2024-06-20 22:27:31 -0300

    refactor

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,38 @@
-import { Text, Image, View, StyleSheet } from "react-native"
-import TextWhite from "./textWhite"
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default Header = (props) => {
-    const {texto , url} = props
-
-    return(
-        <View>
-            <TextWhite texto = {texto} size = {20} />
-            <Image style = {styles.img} source={require('../../assets/IMAGENES/NASA_logo.png')}/>
-        </View>
-    )
-}
+  const { texto, src } = props;
+  return (
+    <View style={styles.container}>
+      <View style={styles.titulo}>
+        <Text style={styles.text}>{texto}</Text>
+      </View>
+      <View style={styles.imgcontainer}>
+        <Image source={require("../../assets/logonasa.png")} style={styles.img}/>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    img: {
-        width: 100,
-        height: 100,
-    }
-})
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  img: {
+    width: 60,
+    height: 60,
+  },
+  titulo: {
+    flex: 1,
+    alignItems: "flex-start",
+  },
+  imgcontainer: {
+    flex: 1,
+    alignItems: "flex-end",
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+  },
+});
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,23 +1,25 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { primaryColor } from '../constants/colors';
-import { NavigatorContainer } from "@react-navigation/native";
 import Home from "../screens/home";
-import Detalle from "../screens/details";
+import Details from "../screens/details";
 
 const Stack = createNativeStackNavigator()
 const optionsDefault = {
     headerStyle: {
-        backgroundColor: primaryColor,
+        backgroundColor: "rgb(25,26,93)",
     },
     headerTitleColor: {color: "blue", alignment : "center"},
     headerTitleAlign: "center",
 }
 
 export default Router = () => {
-    return <NavigatorContainer>
+    return (
+      <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} options={optionsDefault} />
-            <Stack.Screen name="Details" component={Details} options={{title: "inicio" , ...optionsDefault}} />
+          <Stack.Screen name="Home" component={Home} options={optionsDefault}/>
+          <Stack.Screen name="Detalles" component={Details} options={optionsDefault}/>
         </Stack.Navigator>
-    </NavigatorContainer>
-}
+      </NavigationContainer>
+    );
+  };
